bbfabd09f4797f011f1f9d0133dc7a26c7bbc43a
Lucas Alves <lucastouram@gmail.com>
2025-05-11 17:48:59 -0300

    Criando menu e iniciando a criação das classes dos alunos

---
+++ sigaa/src/Sistema.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public void main() {
+        int opcaoSelecionada = 0;
+        do {
+            System.out.println("======MENU PRINCIPAL========");
+            System.out.println("1.Aluno");
+            System.out.println("2.Disciplinas/Turmas");
+            System.out.println("3.Avaliações/Frequências");
+            System.out.println("4.Sair");
+            System.out.print("Escolha uma Opção:");
+            opcaoSelecionada = leitorNumero();
+
+            switch (opcaoSelecionada) {
+                case 1:
+                    menuAluno();
+                    break;
+                case 2:
+                    menuDisciplina();
+                    break;
+                case 3:
+                    System.out.println("O que deseja?");
+
+                    break;
+                case 4:
+                    System.out.println("Salvando alterações e saindo....");
+                default:
+                    System.out.println("Opção Inválida!");
+            }
+        } while (opcaoSelecionada !=4);
+}
+public static void menuAluno() {
+    int opcaoAluno = 0;
+    do {
+        System.out.println("***Menu Aluno***");
+        System.out.println("1.Aluno Normal");
+        System.out.println("2.Auno Especial");
+        System.out.println("3.Voltar");
+        System.out.print("Escolha a opção desejada:");
+        opcaoAluno = leitorNumero();
+
+        switch (opcaoAluno) {
+            case 1:
+                //
+                break;
+            case 2:
+
+                break;
+
+            case 3:
+                System.out.println("Voltando...");
+                break;
+            default:
+                System.out.println("Opção Inválida");
+        }
+    } while (opcaoAluno !=3);
+}
+
+public static void menuDisciplina() {
+    int opcaoDisciplina = 0;
+    do {
+        System.out.println("1.Cadastrar Disciplinas");
+        System.out.println("2.Listar Turmas");
+        System.out.println("3.Voltar");
+        System.out.print("O que deseja?");
+        opcaoDisciplina = leitorNumero();
+        switch (opcaoDisciplina) {
+            case 1:
+                //cadstrar disciplinas
+                break;
+            case 2:
+                //listar Turmas
+                break;
+            case 3:
+                System.out.println("Voltando...");
+        }
+    } while (opcaoDisciplina !=3);
+}
+
+public static int leitorNumero() {
+    int numero = 0;
+    Scanner leitor = new Scanner(System.in);
+    numero = leitor.nextInt();
+    return numero;
+}
